da12d92541f7e3e0fb404d7b4ccd3d158ee6251d
Aru428 <sohee5997@naver.com>
2023-12-28 15:18:27 +0900

    Feat: 댓글 테이블 sequelize

---
+++ backend/model/Comment.js
@@ -0,0 +1,32 @@
+function Comment(Sequelize, DataTypes) {
+  return Sequelize.define(
+    "Comment",
+    {
+      comment_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      board_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      comment_content: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "Comment",
+      frezzeTableName: true,
+      timestamps: false,
+    }
+  );
+}
+
+module.exports = Comment;
--- backend/model/index.js
+++ backend/model/index.js
@@ -14,12 +14,29 @@ db.Sequelize = Sequelize;
 
 db.User = require("./User")(sequelize, Sequelize);
 db.Board = require("./Board")(sequelize, Sequelize);
+db.Comment = require("./Comment")(sequelize, Sequelize);
 
-// 회원 -> 게시판 join
+// 게시판 관련 join
 db.User.hasMany(db.Board, {
   foreignKey: "user_id",
 });
 db.Board.belongsTo(db.User, {
+  ondelete: "cascade",
+  foreignKey: "user_id",
+});
+
+// 댓글 관련 join
+db.Board.hasMany(db.Comment, {
+  foreignKey: "board_id",
+});
+db.Comment.belongsTo(db.Board, {
+  ondelete: "cascade",
+  foreignKey: "board_id",
+});
+db.User.hasMany(db.Comment, {
+  foreignKey: "user_id",
+});
+db.Comment.belongsTo(db.User, {
   ondelete: "cascade",
   foreignKey: "user_id",
 });
