4ac33457ea67c9ef461d3fba4a7243746a0f6e36
DJ94DJ <unistudio94@gmail.com>
2023-12-28 17:30:00 +0900

    DJ: react install

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Shelter from "./components/pages/shelter/Shelter";
+
+function App() {
+  return (
+    <>
+      <Shelter />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/pages/shelter/Shelter.js
@@ -0,0 +1,17 @@
+export default function Shelter() {
+  return (
+    <>
+      <h3></h3>
+      <h1>🐶유기견 공고</h1>
+
+      <form name="serchDogs" action="" method="post">
+        <h3>🔍</h3>
+        <input
+          type="text"
+          name="where"
+          placeholder="지역명을 입력해주세요."
+        ></input>
+      </form>
+    </>
+  );
+}
