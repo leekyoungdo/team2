bf99facc2194e253cf2189d752441813651a1368
DJ94DJ <unistudio94@gmail.com>
2023-12-30 01:17:22 +0900

    shelter 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Shelter from './pages/shelter/Shelter';
-import SignUp from './pages/signup/SignUp';
-import SignIn from './pages/signin/SignIn';
-import Home from './pages/home/Home';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shelter from "./pages/shelter/Shelter";
+import SignUp from "./pages/signup/SignUp";
+import SignIn from "./pages/signin/SignIn";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <main>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/user/signup" element={<SignUp />} />
-          <Route path="/user/signin" element={<SignIn />} />
-          <Route path="/shelter" element={<Shelter />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <main>
+    //     <Routes>
+    //       {/* <Route path="/" element={<Home />} /> */}
+    //       <Route path="/user/signup" element={<SignUp />} />
+    //       <Route path="/user/signin" element={<SignIn />} />
+    //       <Route path="/shelter" element={<Shelter />} />
+    //     </Routes>
+    //   </main>
+    // </BrowserRouter>
+
+    <Shelter />
   );
 }
 
