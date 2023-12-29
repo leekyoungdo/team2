dde3e7ce4c2e4343ed01cbc197d5cf36d624e88b
ldw0123 <ldw0123@gmail.com>
2023-12-29 16:27:27 +0900

    [ADD] - SignUp.js / SignIn.js 수정

--- frontend/src/pages/signin/SignIn.js
+++ frontend/src/pages/signin/SignIn.js
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
 import './signin.scss';
 
 export default function SignIn() {
-  return <></>;
+  return (
+    <>
+      <h2>로그인</h2>
+      <hr />
+      <input type="text" placeholder="아이디를 입력해주세요" />
+      <br />
+      <br />
+      <input type="password" placeholder="비밀번호를 입력해주세요" />
+      <br />
+      <br />
+      <button>로그인</button>
+      <br />
+      <br />
+      <Link to="/user/signup">
+        <button>회원가입</button>
+      </Link>
+    </>
+  );
 }
