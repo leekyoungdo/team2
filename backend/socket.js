f3d9a5dfccc380ee862dd6e484e851870a5ea652
Aru428 <sohee5997@naver.com>
2024-01-02 15:22:12 +0900

    fix: 소켓 사용할 때 채팅방 생성 -> 채팅방 입장으로 교체

--- backend/socket.js
+++ backend/socket.js
@@ -8,12 +8,13 @@ module.exports = (server) => {
   io.on("connection", (socket) => {
     console.log("socket id", socket.id);
 
-    // 채팅방 생성
-    socket.on("createChat", (res) => {
-      console.log("client data", res);
-      socket.join(res.chat_name);
-      io.to(res.chat_name).emit("notice", {
-        msg: `${res.chat_name}이 생성되었습니다!`,
+    // 채팅방 입장
+    socket.on("entry", (res) => {
+      console.log("채팅방 입장", res);
+      const { chat_name, chat_category, nickname } = res;
+      socket.join(chat_name);
+      io.to(chat_name).emit("notice", {
+        msg: `${nickname}님이 입장하셨습니다.`,
       });
     });
   });
