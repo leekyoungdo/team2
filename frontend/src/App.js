3368fce40fdd92bc6cafb279e1407328e87b83ab
ldw0123 <ldw0123@gmail.com>
2024-01-02 17:59:17 +0900

    fix: 회원가입  버그 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ShelterBoard from './pages/12_shelterboard/ShelterBoard';
 import SignUp from './pages/6_signup/SignUp';
 import SignIn from './pages/7_signin/SignIn';
+import Write from './pages/8_write/Write';
+import Page from './pages/9_page/Page';
+import DmPage from './pages/10_dmpage/DmPage';
+import Dm from './pages/11_dm/dm';
 import Home from './pages/home/Home';
+import ShelterBoard from './pages/12_shelterboard/ShelterBoard';
 import CommunityBoard from './pages/14_communityboard/CommunityBoard';
 import MakeCommunity from './pages/15_makecommunity/MakeCommunity';
 import Community from './pages/16_community/Community';
@@ -35,6 +39,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/user/signup" element={<SignUp />} />
           <Route path="/user/signin" element={<SignIn />} />
+          <Route path="/board/boardsubmit" element={<Write />} />
+          <Route path="/board/getboardId/board_id" element={<Page />} />
+          <Route path="/dmpage" element={<DmPage />} />
+          <Route path="/dm" element={<Dm />} />
           <Route path="/shelterboard" element={<ShelterBoard />} />
           <Route path="/communityboard" element={<CommunityBoard />} />
           <Route
