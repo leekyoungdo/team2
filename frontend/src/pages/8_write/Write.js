581c40c6afd0630eb511487f89d1d2e23eacde89
ldw0123 <ldw0123@gmail.com>
2024-01-04 17:34:00 +0900

    feat: 글쓰기 axios, 유효성검사

--- frontend/src/pages/8_write/Write.js
+++ frontend/src/pages/8_write/Write.js
@@ -3,7 +3,11 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 
 export default function Write() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onValid = (data) => {
     // 전송할 데이터 객체
@@ -14,8 +18,12 @@ export default function Write() {
       image: data.image[0], // 사진은 배열로 전달되므로 첫 번째 요소 사용
     };
 
+    console.log(postData, data);
+
     axios
-      .post(`${process.env.REACT_APP_HOST}/board/boardsubmit`, postData)
+      .post(`${process.env.REACT_APP_HOST}/board/boardsubmit`, postData, {
+        withCredentials: true,
+      })
       .then((res) => {
         console.log(res.data);
       })
@@ -31,7 +39,10 @@ export default function Write() {
       <form onSubmit={handleSubmit(onValid)}>
         <div>
           게시판
-          <div className={styles['board-type-container']}>
+          <div
+            className={styles['board-type-container']}
+            {...register('category')}
+          >
             <select className={styles['board-type-select']}>
               <option>자유게시판</option>
               <option>일상게시판</option>
@@ -46,17 +57,39 @@ export default function Write() {
               type="text"
               className={styles['title-text']}
               placeholder="  제목을 입력해주세요"
+              {...register('title', {
+                required: '제목은 필수로 입력해야 합니다',
+              })}
             />
+            {errors.title && (
+              <small role="alert" className={styles.error}>
+                {errors.title.message}
+              </small>
+            )}
           </div>
           <br />
           <div className={styles['contents']}>
             내용
             <br />
-            <textarea className={styles['contents-text']}></textarea>
+            <textarea
+              className={styles['contents-text']}
+              {...register('content', {
+                required: '내용은 필수로 입력해야 합니다',
+              })}
+            ></textarea>
+            {errors.content && (
+              <small role="alert" className={styles.error}>
+                {errors.content.message}
+              </small>
+            )}
           </div>
           <br />
           <br />
-          <label className={styles['upload-button']} htmlFor="picture">
+          <label
+            className={styles['upload-button']}
+            htmlFor="picture"
+            {...register('image')}
+          >
             사진 업로드
             <input
               {...register('image')}
