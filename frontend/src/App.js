ecccd17b5fd12d30e0b684743cbed02d67f5d95f
ldw0123 <ldw0123@gmail.com>
2024-01-04 16:55:55 +0900

    fix: 에러 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,14 @@ import Write from './pages/8_write/Write';
 import Post from './pages/9_Post/Post';
 
 import Home from './pages/1_home/Home';
-import MyPage from './pages/3_mypage/MyPage'
+import MyPage from './pages/3_mypage/MyPage';
 import UserProfile from './pages/4_userprofile/UserProfile';
-import Board from './pages/5_board/Board'
+import Board from './pages/5_board/Board';
 import Nav from './pages/Nav/Nav';
 
 import DmPage from './pages/10_dmpage/DmPage';
 import Dm from './pages/11_dm/Dm';
 import DmTest from './pages/11_dm/DmTest';
-import Home from './pages/home/Home';
 import CommunityBoard from './pages/14_communityboard/CommunityBoard';
 import MakeCommunity from './pages/15_makecommunity/MakeCommunity';
 import Community from './pages/16_community/Community';
@@ -27,8 +26,8 @@ import NotFound from './pages/20_notfound/NotFound';
 function App() {
   const getData = async () => {
     // npm start 명령어로 실행 시
-    console.log("HOST:", process.env.REACT_APP_HOST); // http://localhost:8000
-    console.log("TEST:", process.env.REACT_APP_ENV_TEST); // develop
+    console.log('HOST:', process.env.REACT_APP_HOST); // http://localhost:8000
+    console.log('TEST:', process.env.REACT_APP_ENV_TEST); // develop
     // # npm run build > # serve -s build 명령어로 실행 시 아래와 같이 출력됨.
     // http://000.000.000.000:8000
     // prod
@@ -43,47 +42,46 @@ function App() {
 
   return (
     <BrowserRouter>
-    <div style={{ display: 'flex', minHeight: '100vh' }}>
-      <Nav style={{ flex: '0 0 300px', minWidth: '300px'}} />
-      <main style={{ flex: 1, overflowY: 'auto', padding: '20px' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/userprofile" element={<UserProfile />} />
-          <Route path="/board" element={<Board />} />
+      <div style={{ display: 'flex', minHeight: '100vh' }}>
+        <Nav style={{ flex: '0 0 300px', minWidth: '300px' }} />
+        <main style={{ flex: 1, overflowY: 'auto', padding: '20px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/userprofile" element={<UserProfile />} />
+            <Route path="/board" element={<Board />} />
 
-          <Route path="/user/signup" element={<SignUp />} />
-          <Route path="/user/signin" element={<SignIn />} />
-          <Route path="/board/write" element={<Write />} />
-          <Route path="/board/post" element={<Post />} />
-          <Route path="/dmpage" element={<DmPage />} />
-          <Route path="/dm" element={<Dm />} />
-          <Route path="/dmtest" element={<DmTest />} />
-          <Route path="/shelterboard" element={<ShelterBoard />} />
-          <Route path="/communityboard" element={<CommunityBoard />} />
-          <Route
-            path="/communityboard/makecommunity"
-            element={<MakeCommunity />}
-          />
-          <Route path="/communityboard/community" element={<Community />} />
-          <Route
-            path="/communityboard/community/communityinnerboard"
-            element={<CommunityInnerBoard />}
-          />
-          <Route
-            path="/communityboard/community/communityinnerboard/communitypage"
-            element={<CommunityPage />}
-          />
-          <Route
-            path="/communityboard/community/CommunityTalk"
-            element={<CommunityTalk />}
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-    </div>
+            <Route path="/user/signup" element={<SignUp />} />
+            <Route path="/user/signin" element={<SignIn />} />
+            <Route path="/board/write" element={<Write />} />
+            <Route path="/board/post" element={<Post />} />
+            <Route path="/dmpage" element={<DmPage />} />
+            <Route path="/dm" element={<Dm />} />
+            <Route path="/dmtest" element={<DmTest />} />
+            <Route path="/shelterboard" element={<ShelterBoard />} />
+            <Route path="/communityboard" element={<CommunityBoard />} />
+            <Route
+              path="/communityboard/makecommunity"
+              element={<MakeCommunity />}
+            />
+            <Route path="/communityboard/community" element={<Community />} />
+            <Route
+              path="/communityboard/community/communityinnerboard"
+              element={<CommunityInnerBoard />}
+            />
+            <Route
+              path="/communityboard/community/communityinnerboard/communitypage"
+              element={<CommunityPage />}
+            />
+            <Route
+              path="/communityboard/community/CommunityTalk"
+              element={<CommunityTalk />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
-    
   );
 }
 
