91ef46af97f1cb5694489aa2cff030adde914f0b
sohee <sohee5997@naver.com>
2024-01-08 01:26:07 +0900

    add: home 인기글 추가

--- frontend/src/pages/1_home/Home.js
+++ frontend/src/pages/1_home/Home.js
@@ -8,58 +8,8 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const [dogs, setDogs] = useState([]);
-  const [allboardList, setAllBoardList] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const bannerDogsRef = useRef(null);
-
-  const sampleData1 = [
-    {
-      board: "자유",
-      title: "남산 반려견 산책로 추천 모음",
-    },
-    {
-      board: "자유",
-      title: "참새뗴 발견!",
-    },
-    {
-      board: "일상",
-      title: "강아지 사료 뭐가 좋나요?",
-    },
-    {
-      board: "일상",
-      title: "강아지 훈련법 알려주세요!",
-    },
-    {
-      board: "자유",
-      title: "늙은 강아지랑 노는 법 공유합니다",
-    },
-    {
-      board: "일상",
-      title: "반려견과 함께하는 일상",
-    },
-  ];
-  const sampleData2 = [
-    {
-      author: "콩국수마니아",
-      title: "함께한 산책 기록",
-      content: "오늘 날이 너무 추워서 패딩 입혀봤어요.",
-    },
-    {
-      author: "멍멍이집사",
-      title: "찰떡이는 훈련 중",
-      content: "강아지 훈련에 성공한 비법을 알려드립니다.",
-    },
-    {
-      author: "루돌프",
-      title: "강아지와 함께하는 겨울",
-      content: "겨울에 강아지와 함께하는 재미있는 액티비티를 소개합니다.",
-    },
-    {
-      author: "댕댕이사랑",
-      title: "강아지 생식 냠냠!",
-      content: "너무 잘먹어서 뿌듯해요.",
-    },
-  ];
+  const [newBoard, setNewBoard] = useState([]);
+  const [hotBoard, setHotBoard] = useState([]);
 
   const getApi = () => {
     axios
@@ -75,7 +25,12 @@ export default function Home() {
 
   const getBoard = () => {
     axios.get(`${process.env.REACT_APP_HOST}/board/getallboard`).then((res) => {
-      setAllBoardList(res.data.posts);
+      console.log("getboard", res);
+      setNewBoard(res.data.posts.slice(0, 5));
+      const sortedPosts = res.data.posts.sort(
+        (a, b) => b.viewcount - a.viewcount
+      );
+      setHotBoard(sortedPosts.slice(0, 5));
     });
   };
 
@@ -110,8 +65,8 @@ export default function Home() {
 
         <h2>최신글</h2>
         <div className={`${styles.Box} ${styles.hotTopic}`}>
-          {allboardList.length > 0 &&
-            allboardList.slice(1, 6).map((value) => (
+          {newBoard.length > 0 &&
+            newBoard.map((value) => (
               <Link
                 to={`/board/${value.board_id}`}
                 key={value.board_id}
@@ -126,12 +81,32 @@ export default function Home() {
             ))}
         </div>
 
-        <Link to={`/board/write`}>
+        <br />
+
+        <h2>인기글</h2>
+        <div className={`${styles.Box} ${styles.hotTopic}`}>
+          {hotBoard.length > 0 &&
+            hotBoard.map((value) => (
+              <Link
+                to={`/board/${value.board_id}`}
+                key={value.board_id}
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                <div className={styles.bar} key={value.board_id}>
+                  {value.category}
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  {value.title}
+                </div>
+              </Link>
+            ))}
+        </div>
+
+        {/* <Link to={`/board/write`}>
           <div className={styles.writeButton}>
             <img src={plus} className={styles.plusPic} />
             글쓰기
           </div>
-        </Link>
+        </Link> */}
       </div>
     </>
   );
