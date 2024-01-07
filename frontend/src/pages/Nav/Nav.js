db6ba0923c6c235ed976acfeb8e80ebb7fc09cae
ldw0123 <ldw0123@gmail.com>
2024-01-07 23:06:53 +0900

    rename: 파일명 맨 앞에 _ 추가

--- frontend/src/pages/Nav/Nav.js
+++ frontend/src/pages/Nav/Nav.js
@@ -1,23 +1,34 @@
-import styles from "./Nav.module.scss";
-import React, { useState, useRef, useEffect } from "react";
+import styles from './_Nav.module.scss';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 // import { useSelector } from 'react-redux';
 
 export default function Nav() {
   // const isLoggedIn = useSelector(state => state.nicknameReducer !== '');
 
-
-    return(
-        <>
-        <div className={styles.nav}>
-          <div className={styles.navContainer}>
-            <ul>
-              <div className={styles.logo} ><a href="/">멍멍투게더</a></div>
-              <li><a className={styles.home} href="/">홈</a></li>
-              <li><a href="/board">게시판</a></li>
-              <li><a href="/communityboard">소모임</a></li>
-              <li><a href="/shelterboard">유기견 공고</a></li>
-{/* 
+  return (
+    <>
+      <div className={styles.nav}>
+        <div className={styles.navContainer}>
+          <ul>
+            <div className={styles.logo}>
+              <a href="/">멍멍투게더</a>
+            </div>
+            <li>
+              <a className={styles.home} href="/">
+                홈
+              </a>
+            </li>
+            <li>
+              <a href="/board">게시판</a>
+            </li>
+            <li>
+              <a href="/communityboard">소모임</a>
+            </li>
+            <li>
+              <a href="/shelterboard">유기견 공고</a>
+            </li>
+            {/* 
               {isLoggedIn ? (
                 <li className={styles.signin}>
                   <a href="/user/logout">로그아웃</a>
@@ -27,12 +38,10 @@ export default function Nav() {
                   <a href="/user/signin">로그인</a>
                 </li>
               )} */}
-
-            </ul>
-          </div>
-          <div className={styles.navShadow} />
+          </ul>
         </div>
-        </> 
-    );
-  }
-  
+        <div className={styles.navShadow} />
+      </div>
+    </>
+  );
+}
