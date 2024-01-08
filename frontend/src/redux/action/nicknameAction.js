eabd6621e0a734ba6a1390412e83d3e80238a7f4
Aru428 <sohee5997@naver.com>
2024-01-08 14:34:35 +0900

    add: 로그인 시 redux 생성

--- frontend/src/redux/action/nicknameAction.js
+++ frontend/src/redux/action/nicknameAction.js
@@ -1,8 +1,24 @@
 // Redux Action
 // 닉네임
-export const SET_NICKNAME = 'SET_NICKNAME';
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const UPDATE_USER = "UPDATE_USER";
+export const LOGOUT = "LOGOUT";
+export const DELETE_SUCCESS = "DELETE_USER";
 
-export const setNickname = (nickname) => ({
-  type: SET_NICKNAME,
-  payload: nickname,
+export const loginSuccess = (userData) => ({
+  type: LOGIN_SUCCESS,
+  payload: userData,
+});
+
+export const updateUser = (userData) => ({
+  type: UPDATE_USER,
+  payload: userData,
+});
+
+export const logout = () => ({
+  type: LOGOUT,
+});
+
+export const deleteSuccess = () => ({
+  type: DELETE_SUCCESS,
 });
