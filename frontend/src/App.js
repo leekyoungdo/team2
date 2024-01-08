4b46a5783ec769359be8e901d6fb9c3468b3a810
ldw0123 <ldw0123@gmail.com>
2024-01-08 19:29:20 +0900

    feat: 댓글 등록 기능

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,41 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 // import { composeWithDevTools } from 'redux-devtools-extension';
 // import rootReducer from './redux/reducer';
 
-import Home from "./pages/1_home/Home";
-import MyPage from "./pages/3_mypage/MyPage";
-import UserProfile from "./pages/4_userprofile/UserProfile";
-import Board from "./pages/5_board/Board";
-import Nav from "./pages/Nav/Nav";
+import Home from './pages/1_home/Home';
+import MyPage from './pages/3_mypage/MyPage';
+import UserProfile from './pages/4_userprofile/UserProfile';
+import Board from './pages/5_board/Board';
+import Nav from './pages/Nav/Nav';
 
-import SignUp from "./pages/6_signup/SignUp";
-import SignIn from "./pages/7_signin/SignIn";
-import Write from "./pages/8_write/Write";
-import Post from "./pages/9_Post/Post";
-import DmPage from "./pages/10_dmpage/DmPage";
-import Dm from "./pages/11_dm/dm";
+import SignUp from './pages/6_signup/SignUp';
+import SignIn from './pages/7_signin/SignIn';
+import Write from './pages/8_write/Write';
+import Post from './pages/9_Post/Post';
+import DmPage from './pages/10_dmpage/DmPage';
+import Dm from './pages/11_dm/dm';
 
-import ShelterBoard from "./pages/12_shelterboard/ShelterBoard";
-import CommunityBoard from "./pages/14_communityboard/CommunityBoard";
-import MakeCommunity from "./pages/15_makecommunity/MakeCommunity";
-import Community from "./pages/16_community/Community";
-import CommunityInnerBoard from "./pages/17_communityinnerboard/CommunityInnerBoard";
-import CommunityWrite from "./pages/18_communitywrite/CommunityWrite";
-import CommunityPage from "./pages/17.1_communitypage/CommunityPage";
-import CommunityTalk from "./pages/19_communitytalk/CommunityTalk";
-import NotFound from "./pages/20_notfound/NotFound";
+import ShelterBoard from './pages/12_shelterboard/ShelterBoard';
+import CommunityBoard from './pages/14_communityboard/CommunityBoard';
+import MakeCommunity from './pages/15_makecommunity/MakeCommunity';
+import Community from './pages/16_community/Community';
+import CommunityInnerBoard from './pages/17_communityinnerboard/CommunityInnerBoard';
+import CommunityWrite from './pages/18_communitywrite/CommunityWrite';
+import CommunityPage from './pages/17.1_communitypage/CommunityPage';
+import CommunityTalk from './pages/19_communitytalk/CommunityTalk';
+import NotFound from './pages/20_notfound/NotFound';
 
 // const store = configureStore({ reducer: rootReducer }, composeWithDevTools());
 function App() {
   return (
     // <Provider store={store}>
     <BrowserRouter>
-      <div style={{ display: "flex", maxHeight: "100vh" }}>
+      <div style={{ display: 'flex', maxHeight: '100vh' }}>
         <Nav />
-        <main style={{ flexGrow: 1, overflowY: "auto" }}>
+        <main style={{ flexGrow: 1, overflowY: 'auto' }}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/mypage" element={<MyPage />} />
@@ -63,7 +63,7 @@ function App() {
               element={<CommunityWrite />}
             />
             <Route
-              path="/communityboard/community/communityinnerboard/communitypage/:pageNum"
+              path="/communityboard/community/communityinnerboard"
               element={<CommunityPage />}
             />
             <Route
