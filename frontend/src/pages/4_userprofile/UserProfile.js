b64003a8f49187bed7de5c77bdeb3f07a7e6a5e8
J-Yeonju <Yeonju.dev@gmail.com>
2024-01-09 17:05:28 +0900

    design: userprofile UI 수정

--- frontend/src/pages/4_userprofile/UserProfile.js
+++ frontend/src/pages/4_userprofile/UserProfile.js
@@ -47,90 +47,70 @@ export default function UserProfile() {
     <>
       <div className={styles.profileContainer}>
         <h2 className={styles.header}>프로필</h2>
+        <div className={styles.divisionLine}></div>
 
-        <div className={styles.picture}>
-          <img
-            src={`${process.env.REACT_APP_HOST}${userInfo && userInfo.image}`}
-            alt="프로필 사진"
-          />
+        <div className={styles.topBox}>
+          <div className={styles.infoBox}>
+            <div className={styles.picture}>
+              <img
+                src={`${process.env.REACT_APP_HOST}${userInfo && userInfo.image}`}
+                alt="프로필 사진"
+              />
+            </div>
+
+            <div className={styles.info}>
+              <p className={styles.nickname}>{userInfo && userInfo.nickname}닉네임</p>
+              <p className={styles.introduction}>{userInfo && userInfo.dog_name}불이</p>
+              <p className={styles.introduction}>
+                {userInfo && userInfo.dog_intro}
+                슈나우져 5살
+              </p>
+              <button className={styles.messageButton} onClick={handleDmClick}>
+                쪽지 보내기
+              </button>
+            </div>
+          </div>
         </div>
 
-        <div className={styles.info}>
-          <p className={styles.nickname}>{userInfo && userInfo.nickname}</p>
-          <p className={styles.introduction}>{userInfo && userInfo.dog_name}</p>
-          <p className={styles.introduction}>
-            {userInfo && userInfo.dog_intro}
-          </p>
-          <button className={styles.messageButton} onClick={handleDmClick}>
-            쪽지 보내기
-          </button>
+
+        <div className={styles.boardTableContainer}>
+          <p className={styles.tableTab}>작성 글</p>
+          <table className={styles.boardTable}>
+            <thead>
+              <tr>
+                <th>게시판</th>
+                <th>제목</th>
+                <th>날짜</th>
+                <th>조회수</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>일상</td>
+                <td>행복한 저녁 시간~</td>
+                <td>2024-01-05</td>
+                <td>100</td>
+              </tr>
+              <tr>
+                <td>자유</td>
+                <td>이럴 때는 어쩌죠?</td>
+                <td>2024-01-05</td>
+                <td>100</td>
+              </tr>
+              <tr>
+                <td>일상</td>
+                <td>꼬까옷 입고 폴짝!</td>
+                <td>2024-01-05</td>
+                <td>100</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-
-        <table className={styles.boardTable}>
-          <thead>
-            <tr>
-              <th>게시판</th>
-              <th>제목</th>
-              <th>날짜</th>
-              <th>조회수</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>일상</td>
-              <td>행복한 저녁 시간~</td>
-              <td>2024-01-05</td>
-              <td>100</td>
-            </tr>
-            <tr>
-              <td>자유</td>
-              <td>이럴 때는 어쩌죠?</td>
-              <td>2024-01-05</td>
-              <td>100</td>
-            </tr>
-            <tr>
-              <td>일상</td>
-              <td>꼬까옷 입고 폴짝!</td>
-              <td>2024-01-05</td>
-              <td>100</td>
-            </tr>
-          </tbody>
-        </table>
 
         <br />
 
-        <table className={styles.commentTable}>
-          <thead>
-            <tr>
-              <th>게시판</th>
-              <th>제목</th>
-              <th>댓글</th>
-              <th>날짜</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>일상</td>
-              <td>행복한 저녁 시간~</td>
-              <td>맛있겠다~~</td>
-              <td>2024-01-05</td>
-            </tr>
-            <tr>
-              <td>자유</td>
-              <td>이럴 때는 어쩌죠?</td>
-              <td>저도 그런 경험이....</td>
-              <td>2024-01-05</td>
-            </tr>
-            <tr>
-              <td>일상</td>
-              <td>꼬까옷 입고 폴짝!</td>
-              <td>너무 이뻐요~~</td>
-              <td>2024-01-05</td>
-            </tr>
-          </tbody>
-        </table>
+        
       </div>
     </>
   );
